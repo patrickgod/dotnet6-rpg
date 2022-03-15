1000cb6181803198818fd40ae911266587bb9db2
Patrick <patrick@godknows.de>
2022-03-15 15:48:25 +0100

    Implementing the DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
