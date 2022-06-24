c1d57cc9de354f134722c06baa7cb842185c10bd
Patrick <patrick@godknows.de>
2022-06-25 01:35:57 +0200

    Attack with Weapons

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Fight;
 using dotnet_rpg.Services.FightService;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,12 @@ namespace dotnet_rpg.Controllers
         public FightController(IFightService fightService)
         {
             _fightService = fightService;
+        }
+
+        [HttpPost("Weapon")]
+        public async Task<ActionResult<ServiceResponse<AttackResultDto>>> WeaponAttack(WeaponAttackDto request)
+        {
+            return Ok(await _fightService.WeaponAttack(request));
         }
     }
 }
